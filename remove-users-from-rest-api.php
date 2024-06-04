ac8a1861038dba517fa4e95c1bdd3d0bd6029d2d
Ash Whiting <ash_whiting@hotmail.com>
2024-06-04 17:10:22 +0100

    Initial commit

---
+++ remove-users-from-rest-api.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Plugin Name:        	BrightLocal - Disable REST API User Endpoints
+ * Description:         Disable the Users REST API user endpoint for security reasons.
+ * Author:              Ash Whiting for BrightLocal
+ * Author URI:          https://brightlocal.com
+ * Text Domain:         smntcs-disable-rest-api-user-endpoints
+ * Version:             1.0.3
+ * License:             GPL v2 or later
+ * License URI:         https://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * @package bl_disable_rest_users
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Disable REST API user endpoints.
+ *
+ * @param array $endpoints The original endpoints.
+ * @return array The updated endpoints.
+ * @since 1.0.0
+ */
+function bl_rest_endpoints( $endpoints ) {
+	if ( isset( $endpoints['/wp/v2/users'] ) ) {
+		unset( $endpoints['/wp/v2/users'] );
+	}
+	if ( isset( $endpoints['/wp/v2/users/(?P<id>[\d]+)'] ) ) {
+		unset( $endpoints['/wp/v2/users/(?P<id>[\d]+)'] );
+	}
+
+	return $endpoints;
+}
+add_filter( 'rest_endpoints', 'bl_rest_endpoints' );
